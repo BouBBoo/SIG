49eb6c61774466f18fc57f1776f7c1c3898a31d4
Quentin BOUNY <qbouny@gmail.com>
2020-12-07 19:57:06 +0100

    Add nodejs server

---
+++ openlayers/index.js
@@ -0,0 +1,24 @@
+import 'ol/ol.css';
+import {Map, View} from 'ol';
+import Tile from 'ol/layer/Tile';
+import TileWMS from 'ol/source/TileWMS';
+
+var map = new Map({
+    target: 'map',
+    layers: [
+      new Tile({
+        title: 'Global Imagery',
+        source: new TileWMS({
+          url: 'http://192.168.1.21:8080/geoserver/wms',
+          crossOrigin: 'anonymous' ,
+          params: {LAYERS: 'SIG:SalleE0,SIG:EscalierE0', TILED: true}
+        })
+      })
+    ],
+    view: new View({
+      projection: 'EPSG:4326',
+      center: [0, 0],
+      zoom: 5,
+      maxResolution: 0.703125
+    })
+  });
