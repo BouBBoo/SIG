02e7f8d3839212bdad0ea5be738f7a796e58b016
Quentin BOUNY <qbouny@gmail.com>
2020-12-07 19:56:47 +0100

    Android update

--- AndroidSIG/app/src/main/java/com/example/androidsig/MainActivity.java
+++ AndroidSIG/app/src/main/java/com/example/androidsig/MainActivity.java
@@ -1,18 +1,25 @@
 package com.example.androidsig;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         WebView myWebView = (WebView) findViewById(R.id.webview);
-        myWebView.loadUrl("http://localhost:8080/geoserver/rest/layers/salleE0.html");
+        myWebView.getSettings().setLoadWithOverviewMode(true);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setWebContentsDebuggingEnabled(true);
+        myWebView.getSettings().setUseWideViewPort(true);
+        myWebView.loadUrl("http://192.168.1.21:1234");
     }
 }
